d0aa1c5894563d274ed6c97c221d7b565196f9eb
Dantenauj <naujjuan91@gmail.com>
2022-06-02 22:40:34 -0400

    comprobante

---
+++ src/app/modules/parametros/comprobante/type/configuracion-codigo.types.ts
@@ -0,0 +1,13 @@
+import {Option} from '@angular/cli/models/interface';
+
+export interface ConfiguracionCodigo {
+    id: number;
+    numeroControl: number;
+    codigoCuenta: number;
+    nombre: string;
+    valorPorcentaje: number;
+    valorSeleccion: boolean;
+    valorRadio: boolean;
+    fecha: string;
+}
+
